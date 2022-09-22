2733403f94b11fa5d2d272662a618ce14d7aa39c
apurva3693 <choudhariapurva3@gmail.com>
2022-09-21 21:41:11 -0500

    responsive dashboard

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Apple 11-inch iPad Pro',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Samsung Galaxy S20 FE 5G UW',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Apple Watch Series 7',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Bose QuietComfort 45 Headphones',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Apple Watch Series 7',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Lenovo ThinkPad X13s',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Apple iPhone 13 Pro Max',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
+
